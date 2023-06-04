dfe76688b483276ce7ab5d285f3c31bac3be7b82
czer <czerpdx@gmail.com>
2023-06-03 18:31:37 -0700

    added pseudocode test plan for adding and removing tag from portfolio_tags

---
+++ utilities/dbHandler.test.js
@@ -0,0 +1,18 @@
+// https://jestjs.io/docs/getting-started
+
+const sum = require('./dbHandler');
+
+test('Add and remove a test tag from portfolio_tags table', () => {
+
+  // Verify if the tag exists
+    // If tag already exist
+      // Return error
+
+    // If tag doesn't exist
+      // Add the tag
+      // Verify that it now exists
+        // If it exists
+          // Delete it for cleanup
+        // If it doesn't exist
+          // Return error
+});
